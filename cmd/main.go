525a12f8c34804c3d8ca56b09a3990c766e43099
Nandika <dnsw83@gmail.com>
2025-08-29 22:50:25 +0530

    harden the http server

--- cmd/main.go
+++ cmd/main.go
@@ -22,11 +22,18 @@ func main() {
 	mux := api.NewRouter()
 
 	srv := &http.Server{
-		Addr:    ":8080",
-		Handler: mux,
+		Addr:              ":8080",
+		Handler:           mux,
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      15 * time.Second,
+		IdleTimeout:       60 * time.Second,
+		MaxHeaderBytes:    1 << 20,
 	}
 
 	// Graceful shutdown
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
 	go func() {
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			util.Logger.Error("listen", "error", err)
@@ -35,14 +42,12 @@ func main() {
 	}()
 	util.Logger.Info("server.started", "addr", ":8080")
 
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
+	<-ctx.Done()
 	util.Logger.Info("server.shutting_down")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	if err := srv.Shutdown(ctx); err != nil {
+	if err := srv.Shutdown(shutdownCtx); err != nil {
 		util.Logger.Error("server.shutdown_forced", "error", err)
 		os.Exit(1)
 	}
